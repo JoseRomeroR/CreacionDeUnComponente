de5c322fa066f5e58cf740f885dbdb45605d3b50
JoseRomero <joseromerodev@gmail.com>
2022-11-24 09:31:40 +0100

    Second upload

---
+++ src/Componentes/CampoTextoNumerico.java
@@ -0,0 +1,21 @@
+package Componentes;
+
+import javafx.scene.control.*;
+
+public class CampoTextoNumerico extends TextField {
+
+    @Override
+    public void replaceText(int start, int end, String text) {
+        if (!text.matches("[a-z, A-Z]")) {
+            super.replaceText(start, end, text);
+        }
+        //label.setText("Enter a numeric value");
+    }
+
+    @Override
+    public void replaceSelection(String text) {
+        if (!text.matches("[a-z, A-Z]")) {
+            super.replaceSelection(text);
+        }
+    }
+};
